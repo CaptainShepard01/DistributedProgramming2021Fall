42acb487e9eb1317712846a1f361221b937947fa
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2021-10-07 13:46:51 +0300

    Lab 4 graded

---
+++ src/com/company/lab4/b/lab4b.java
@@ -0,0 +1,64 @@
+package com.company.lab4.b;
+
+public class lab4b {
+    public static void main(String[] args) throws InterruptedException {
+        Garden garden = new Garden(5, 5);
+        Garden.clearFile();
+
+        Thread gardener = new Thread(() -> {
+            for(int i = 0; i < 5; ++i){
+                Garden.pourPlants();
+                System.out.println("Garden has been poured.\n");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread nature = new Thread(() -> {
+            for(int i = 0; i < 10; ++i){
+                Garden.motherNature();
+                System.out.println("Garden has been changed by Mother Nature.\n");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread monitor1 = new Thread(() -> {
+            for(int i = 0; i < 5; ++i){
+                Garden.writeToFile();
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread monitor2 = new Thread(() -> {
+            for(int i = 0; i < 5; ++i){
+                Garden.printGarden();
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        gardener.start();
+        nature.start();
+        monitor1.start();
+        monitor2.start();
+
+        gardener.join();
+        nature.join();
+        monitor1.join();
+        monitor2.join();
+    }
+}
