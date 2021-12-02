81d519e51a308e37e7d13dad4b29f574675ca41c
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2021-12-02 14:28:39 +0200

    Files from GithubDesktop

---
+++ src/main/java/com/company/Module1/lab6/cmake-build-debug-visual-studio/CMakeFiles/mainExec.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainExec.dir/main.cpp.obj"
+  "mainExec.exe"
+  "mainExec.exe.manifest"
+  "mainExec.lib"
+  "mainExec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainExec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
